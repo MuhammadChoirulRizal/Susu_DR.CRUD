0178521c8e70da92efa09ec0a49f398bb58ad938
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2026-04-14 11:37:59 +0700

    membuat koneksi

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -4,5 +4,37 @@ using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
 {
-    }
+    public partial class Form1 : Form
+
+    {
+
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=LAPTOP-5LMNPAS3\\CHOY;Initial Catalog=DBAkademikADO;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                MessageBox.Show("Konneksi Berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Konneksi Gagal:" + ex.Message);
+            }
+        }
+
+       
 }
